7753c8deb12bb7576e3aa9954c05e82a3f9be3bf
lekshmypk <pklekshmi96@gmail.com>
2023-10-04 23:39:08 +0530

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,14 @@ use App\Http\Controllers\FrontendController;
 |
 */
 
-Route::get('/', [FrontendController::class, 'home'])->name('/');
+Route::get('/', [FrontendController::class, 'home']);
 Route::get('/contact', [FrontendController::class, 'contact'])->name('contact');
 Route::get('/about', [FrontendController::class, 'about'])->name('about');
 Route::get('/service', [FrontendController::class, 'service'])->name('service');
 Route::get('/home', [FrontendController::class, 'home'])->name('home');
 Route::get('/signup', [FrontendController::class, 'signup'])->name('signup');
 Route::get('/login', [FrontendController::class, 'login'])->name('login');
-// Route::get('/login',function(){return view('login');});
-// Route::get('/lawyer',function(){return view('lawyer');});
+Route::get('/login',function(){return view('login');});
+Route::get('/lawyer',function(){return view('lawyer');});
 
 ?>
